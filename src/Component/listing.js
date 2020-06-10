e59a28d43b0e4d08fe664895513e666b7fb2c326
gabriel andy fonogit config --global user.email fonoandy <fonoandy@gmail.com>
2020-06-09 20:44:25 -0400

    I completed the inside

--- src/Component/listing.js
+++ src/Component/listing.js
@@ -28,37 +28,105 @@ const Listing = () => {
   `;
   const Article = styled.article`
     background-color: white;
+    &:hover {
+      Address {
+        opacity: 0;
+      }
+    }
   `;
-const Info = styled.div`
-position: relative;
-`
+  const Info = styled.div`
+    position: relative;
+    cursor: pointer;
+  `;
+  const Container = styled.div`
+    opacity: 0;
+    background-color: rgba(78, 2, 229, 0.8);
+    border-radius: 5px;
+    position: absolute;
+    top: 1;
+    right: 0;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    height: 100%;
+    transition: opacity 0.5s ease-in-out;
+    &:hover {
+      opacity: 1;
+    }
+    &:hover {
+      Address {
+        opacity: 0;
+      }
+    }
+  `;
+
   const Address = styled.address`
-position: absolute;
-top:80%;
-left:10%;
-color:white;
-font-size:1.3rem;
-`
+    display: block;
+    position: absolute;
+    opacity: 1;
+    z-index: 4;
+    top: 80%;
+    left: 10%;
+    color: white;
+    height: fit-content;
+    font-size: 1.3rem;
+    transition: all 0.5s ease-in-out;
+  `;
 
   const ImageContainer = styled.div`
-    
     overflow: hidden;
     cursor: pointer;
     height: 20rem;
+    z-index: 3;
   `;
   const Image = styled.img`
     height: 100%;
     width: 100%;
   `;
   const Location = styled.div`
-  margin-left:20px;
-  color:orange;
-`
-   const Span = styled.span`
-     display:inline-block;
-     margin-left:10px;
-     color:black;
-   `
+    margin-left: 20px;
+    color: orange;
+  `;
+  const Span = styled.span`
+    display: inline-block;
+    margin-left: 10px;
+    color: black;
+  `;
+
+  const AgentListing = styled.div`
+    position: absolute;
+    top: 40%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    color: white;
+  `;
+  const AgentDetail = styled.div`
+    display: flex;
+    justify-content: space-between;
+  `;
+  const ImgAgent = styled.div`
+    width: 50px;
+    height: 50px;
+    margin-right: 15px;
+  `;
+  const ImgPhoto = styled.img`
+    width: 100%;
+    height: 100%;
+    border-radius: 50%;
+  `;
+  const ViewMap = styled.p`
+    width: fit-content;
+    padding: 10px;
+    background-color: pink;
+    transition: background-color 0.5s ease-in-out;
+    &:hover {
+      background-color: black;
+    }
+  `;
+  const ImgAgentDetail = styled.div`
+    margin: 0;
+    padding:0;
+  `;
   return (
     <Section>
       <ConTitle>
@@ -67,11 +135,26 @@ font-size:1.3rem;
       <Properties className="all-properties">
         <Article>
           <div className="single-items">
-            <Info >
+            <Info>
               <ImageContainer className="image-container">
-                <Image className="house-img" src="images/img-1.jpg" />
+                <Image src="images/img-1.jpg" />
               </ImageContainer>
               <Address>7598 duneiden lane</Address>
+              <Container>
+                <AgentListing className="detail-listing">
+                  <AgentDetail className="agent-detail">
+                    <ImgAgent>
+                      <ImgPhoto src="images/img-3.jpg" />
+                    </ImgAgent>
+                    <ImgAgentDetail>
+                      <p>gabriel fono</p>
+                      <p>posted on june 29 1991</p>
+                      <span>7 bed rooms</span>
+                      <ViewMap>View listing</ViewMap>
+                    </ImgAgentDetail>
+                  </AgentDetail>
+                </AgentListing>
+              </Container>
             </Info>
             <Location className="location">
               <p>
